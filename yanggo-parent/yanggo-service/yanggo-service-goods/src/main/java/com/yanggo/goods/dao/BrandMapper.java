47953f9a35d56af5c08f378012ce0a274a3b1a65
siyang <zsy15@qq.com>
2020-11-17 16:41:08 +0800

    初始版本

---
+++ yanggo-parent/yanggo-service/yanggo-service-goods/src/main/java/com/yanggo/goods/dao/BrandMapper.java
@@ -0,0 +1,22 @@
+package com.yanggo.goods.dao;
+
+import com.yanggo.goods.pojo.Brand;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 描述
+ *
+ * @author 三国的包子
+ * @version 1.0
+ * @package com.changgou.dao *
+ * @since 1.0
+ */
+
+/**
+ * Mapper<Brand>  指定通用的mapper的父接口:封装了所有的CRUD的操作
+ * T  指定操作的表对应的POJO
+ */
+@Repository
+public interface BrandMapper extends Mapper<Brand> {
+}
